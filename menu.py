b2cef777c702380db3820516af0f26c82ac96d08
richardatl5 <69731327+richardatl5@users.noreply.github.com>
2021-03-12 18:46:10 -0500

    f

--- menu.py
+++ menu.py
@@ -11,7 +11,6 @@ This application is powered by Streamlit, Pandas, Sklearn, & Tableau
 app.add_app("Home", home.app)
 app.add_app("Statistical Insights", data.app)
 app.add_app("Rental Price Predictor", rent.app)
-# app.add_app("Price Predictor with Deep Neural Network", dnn.app)
 app.add_app("Crime vs Listing Price", kim.app)
 app.add_app("Race vs Sales/Rental Price", grant.app)
 app.add_app("Turner & Suntrust Impact", greg.app)
