147f5ca126e11ce21d4580a839140f0d3376418f
richardatl5 <69731327+richardatl5@users.noreply.github.com>
2021-03-12 17:43:21 -0500

    g

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
+
+
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# \n\
+# " > ~/.streamlit/config.toml
